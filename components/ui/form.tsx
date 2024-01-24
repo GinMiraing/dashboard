605148d9a1e9fb2b4dd7eea4c4b4278adbc4fc7e
Gin Miraing <ginmiraing@outlook.com>
2024-01-25 00:14:35 +0800

    feat: add shadcn components

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -139,7 +139,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-muted-foreground text-sm", className)}
+      className={cn("text-sm text-muted-foreground", className)}
       {...props}
     />
   );
@@ -161,7 +161,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-destructive text-sm font-medium", className)}
+      className={cn("text-sm font-medium text-destructive", className)}
       {...props}
     >
       {body}
