c9cac09dabef38789956c0de14a95fb83f4688e1
Gin Miraing <ginmiraing@outlook.com>
2024-01-28 01:32:33 +0800

    feat: sync type

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,37 @@
-export type CommentSchemaType = {
+import { z } from "zod";
+
+export const CommentUpdateSchema = z.object({
+  nick: z.string().min(1),
+  email: z.string().email(),
+  email_md5: z.string().length(32),
+  link: z.union([z.string().length(0), z.string().url()]),
+  content: z.string().min(1),
+  is_admin: z.union([z.literal(1), z.literal(0)]),
+  is_hidden: z.union([z.literal(1), z.literal(0)]),
+  timestamp: z.number(),
+  reply_count: z.number().min(0),
+  path: z.string().min(1),
+  parent_id: z.number().min(0),
+  reply_id: z.number().min(0),
+  reply_nick: z.union([z.string().length(0), z.string().min(1)]),
+});
+
+export const CommentCreateSchema = CommentUpdateSchema.pick({
+  nick: true,
+  email: true,
+  link: true,
+  content: true,
+  path: true,
+  parent_id: true,
+  reply_id: true,
+  reply_nick: true,
+});
+
+export type CommentUpdateSchemaType = z.infer<typeof CommentUpdateSchema>;
+
+export type CommentCreateSchemaType = z.infer<typeof CommentCreateSchema>;
+
+export type CommentPrismaType = {
   id: number;
   nick: string;
   email: string;
