1a0d1849accc7df8fe779e3991cd83d8ca1aa566
Gin Miraing <ginmiraing@outlook.com>
2024-02-02 02:38:08 +0000

    feat: check session before login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,23 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import RedisClient from "@/lib/redis";
+
 import LoginCard from "./components/loginCard";
 
-export default function Page() {
+export default async function Page() {
+  const session = cookies().get("SESSION");
+
+  if (session) {
+    const userData = await RedisClient.get(session.value);
+
+    if (userData) {
+      return redirect("/dashboard");
+    }
+  }
+
   return (
     <main className="flex h-screen w-full items-center justify-center">
       <LoginCard />
